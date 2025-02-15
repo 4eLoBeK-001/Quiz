7f21549ecf5d55c103a18e63d50aa4a31330b152
Konstantin Efanov <4eLoBeK0012@gmail.com>
2025-02-15 17:26:15 +0500

    main page

--- quiz/quiz/urls.py
+++ quiz/quiz/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('quiz_app.urls')),
-    path('/', include('user.urls')),
+    path('', include('quiz_app.urls')),
+    path('', include('user.urls')),
 ]
--- quiz/quiz_app/urls.py
+++ quiz/quiz_app/urls.py
@@ -1,4 +1,7 @@
-from django.urls import include
+from django.urls import path
+
+from quiz_app import views
 
 urlpatterns = [
+    path('home/', views.home)
 ]
